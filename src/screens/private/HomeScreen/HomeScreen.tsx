7d3bf5ea02fff81920372a42de866d57d47ed808
Kranthi <kranthi@sumeruinfras-MacBook-Pro.local>
2021-06-28 21:30:02 +0530

    feat: show movie's based on age

--- src/screens/private/HomeScreen/HomeScreen.tsx
+++ src/screens/private/HomeScreen/HomeScreen.tsx
@@ -105,32 +105,30 @@ const HomeScreen = ({ authState, movieState }: any) => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Container fluid>
-        <div className="movies">
-          <h3>Popular Movies</h3>
-          <Row>
-            {movies.map((movie: Movie) => {
-              return <MovieTile key={movie.id} movie={movie} />;
-            })}
-          </Row>
-          <Row className="d-flex justify-content-center mt-3">
-            {currentPage < totalPages && (
-              <Row className="col-lg-2 col-sm-6">
-                <LoadingButton
-                  text="Load more"
-                  type="submit"
-                  variant="movie-planet"
-                  loadingText="Loading..."
-                  isLoading={isFetching}
-                  size="lg"
-                  block={true}
-                  disabled={isFetching}
-                  onClick={() => loadMoreMovies()}
-                />
-              </Row>
-            )}
-          </Row>
-        </div>
+      <Container fluid className="movies">
+        <h3>Popular Movies</h3>
+        <Row>
+          {movies.map((movie: Movie) => {
+            return <MovieTile key={movie.id} movie={movie} />;
+          })}
+        </Row>
+        <Row className="d-flex justify-content-center mt-3">
+          {currentPage < totalPages && (
+            <Row className="col-lg-2 col-sm-6">
+              <LoadingButton
+                text="Load more"
+                type="submit"
+                variant="movie-planet"
+                loadingText="Loading..."
+                isLoading={isFetching}
+                size="lg"
+                block={true}
+                disabled={isFetching}
+                onClick={() => loadMoreMovies()}
+              />
+            </Row>
+          )}
+        </Row>
       </Container>
     </>
   );
